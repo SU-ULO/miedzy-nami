f7afe995ce36e4221456f183783ed61f7294451c
NNX <24442148+TheNNX@users.noreply.github.com>
2021-01-22 18:29:50 +0100

    Add TaskExampleGUI.cs

---
+++ scripts/tasks/taskexamplegui/TaskExampleGUI.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public class TaskExampleGUI : TaskWithGUI
+{
+	public virtual string GetResourceLocation(){
+		return "res://gui/NAME OF THE GUI SCENE GOES HERE.tscn";
+	}
+}
