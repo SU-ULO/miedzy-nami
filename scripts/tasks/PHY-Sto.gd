24f64df9c4ff95e3b0ac23942ff03fa8d75788c9
PangolinGiLA <wojtek.j.malecha@gmail.com>
2021-01-26 13:09:12 +0100

    new task

---
+++ scripts/tasks/PHY-Sto.gd
@@ -0,0 +1,25 @@
+extends Control
+
+var pos = 0
+var pos2 = 0
+var started = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_node("AnimationPlayer").play("wahadlo")
+
+
+func _on_TextureButton_pressed():
+	get_node("AnimationPlayer2").play("clock-tick")
+	if !started:
+		started = true
+		pos = get_node("AnimationPlayer").current_animation_position	
+	else:
+		started = false
+		get_node("AnimationPlayer2").stop()
+		pos2 = get_node("AnimationPlayer").current_animation_position
+		if abs(pos - pos2) < 0.1 || abs(pos - (get_node("AnimationPlayer").current_animation_length - pos2)) < 0.1:
+			print("Gratulacje!") # only for testing
+			#end task
+		else:
+			#ups zrob to jeszcze raz (jak to zasygnalizowac graczowi??)
+			print("Nie") #only for testing
---
+++ scripts/tasks/PHYSto.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public class PHYSto : TaskWithGUI
+{
+	public override string GetResourceLocation(){
+		return "res://gui/PHY-Sto.tscn";
+	}
+}
