6a9dbbf1f7fe3eca839c83c91d00125009e8a6ea
PangolinGiLA <wojtek.j.malecha@gmail.com>
2021-02-25 21:46:04 +0100

    more character customisation

--- entities/character-customisation/look-configuration.gd
+++ entities/character-customisation/look-configuration.gd
@@ -7,7 +7,7 @@ var eye = "neutral_open"
 var eye_color = "darkblue"
 var hair = "short hair"
 var hairColor = 1
-var hairPos = {"Velma long hair": 384, "Velma short hair":266, "afro":195, "bob cut":239, "curly afroish":225, "fade":248, "fade2":195, "kok":179, "koki":242, "long hair":379, "ponytail":192, "ponytails":230, "puffy hair":240, "short hair":240, "very short":283}
+var hairPos = {"Velma long hair": 384, "Velma short hair":266, "afro":195, "bob cut":239, "curly afroish":225, "fade":248, "fade2":195, "kok":179, "koki":242, "long hair":379, "ponytail":192, "ponytails":230, "puffy hair":240, "short hair":240, "very short":283, "jheri curl":248, "short hair 2": 212, "wavy long hair":378, "wavy short hair":280, "bald":0}
 
 var hasSkinColorMouth = ["cheeks mouth closed", "cheeky smile semi-closed", "chewing smile closed", "miserable closed", "old smile", "sad closed", "smile closed", "smug smile closed", "wide smile closed"]
 var hasBonusEyes = ["bored_open", "clumsy_open", "old_open", "sad_open", "smug_open", "suspicious_open", "suspicious_semi-open", "winking"]
