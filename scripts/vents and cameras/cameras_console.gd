f6a9120b7cac5c8393e9c7fee9c37276df863693
Sobgo <60814050+Sobgo@users.noreply.github.com>
2021-02-28 23:21:36 +0100

    dirty error fix

--- scripts/vents and cameras/cameras_console.gd
+++ scripts/vents and cameras/cameras_console.gd
@@ -9,18 +9,26 @@ func _ready():
 	self.add_to_group("entities")
 	self.add_to_group("interactable")
 	self.add_to_group("cameras")
-	prep_gui()
 
-func prep_gui():
-	var iter = 0
+func delete_gui():
+	for lc in linkedcameras:
+		get_node(lc).get_node("Camera2D").current = 0
+	canvas.remove_child(camera_gui)
+
+func instance_gui():
 	for vp in camera_gui.viewports:
 		camera_gui.get_node(vp).world_2d = world
+		
+	canvas.add_child(camera_gui)
+	var iter = 0
+	
+	for vp in camera_gui.viewports:
 		get_node(linkedcameras[iter]).get_node("Camera2D").set_custom_viewport(camera_gui.get_node(vp))
-		get_node(linkedcameras[iter]).get_node("Camera2D").current = true
+		get_node(linkedcameras[iter]).get_node("Camera2D").current = 1
 		iter += 1
 
 func open(_body):
-	canvas.add_child(camera_gui)
+	instance_gui()
 
 func close(_body):
-	canvas.remove_child(camera_gui)
+	delete_gui()
