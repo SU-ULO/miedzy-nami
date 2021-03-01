5f1c2c3fe2e440c720ba6123cdbe36312d722f56
PangolinGiLA <wojtek.j.malecha@gmail.com>
2021-03-01 11:45:33 +0100

    add task to map

--- czyscik.gd
+++ czyscik.gd
@@ -7,11 +7,14 @@ var inProbowka = false
 var clickDelta
 var cleared = false
 var toClear = [2, 2]
+var bruud=[[],[]]
 var cleaning = 0
 var ended = false
 func _ready():
-	pass # Replace with function body.
-
+	bruud[0].append(get_parent().get_node("brud"))
+	bruud[0].append(get_parent().get_node("brud2"))
+	bruud[1].append(get_parent().get_node("brud3"))
+	bruud[1].append(get_parent().get_node("brud4"))
 func _process(_delta):
 	if ($czubek.global_position - get_parent().get_node("p1").position).length() < ($czubek.global_position - get_parent().get_node("p2").position).length():
 		closer = get_parent().get_node("p1")
@@ -38,6 +41,8 @@ func _process(_delta):
 			if cleared && $czubek.global_position.x > closer.get_node("k").global_position.x + 400:
 				cleared = false
 				#wyczysc brud
+				if toClear[cleaning] > 0:
+					bruud[cleaning][toClear[cleaning]-1].visible = false
 				toClear[cleaning]-=1
 		else:
 			rect_global_position = get_viewport().get_mouse_position()  - clickDelta
@@ -58,4 +63,7 @@ func _on_czyscik_button_up():
 	dragging = false
 
 func endpls():
-	print("yay")
+		get_parent().get_node("Timer").start()
+		yield(get_parent().get_node("Timer"), "timeout")
+		var TaskWithGUI = load("res://scripts/tasks/TaskWithGUI.cs")
+		TaskWithGUI.TaskWithGUICompleteTask(get_parent())
---
+++ scripts/tasks/CHEczyszczenie.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public class CHEczyszczenie : TaskWithGUI
+{
+	public override string GetResourceLocation(){
+		return "res://gui/CHE-czyszczenie.tscn";
+	}
+}
