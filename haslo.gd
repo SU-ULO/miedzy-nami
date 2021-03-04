42b875984c32c5e44dda3b397755051811f6ce09
PangolinGiLA <wojtek.j.malecha@gmail.com>
2021-03-04 20:15:23 +0100

    new task

---
+++ haslo.gd
@@ -0,0 +1,29 @@
+extends Control
+
+var passwords = ["ulo2021", "ULOulo", "uloULO", "ULO21", "ulo21", "21ULO"]
+var correct
+
+var buffer = ""
+
+func _ready():
+	$input/Label.text = ""
+	randomize()
+	passwords.shuffle()
+	correct = passwords.front()
+	$pass/Label.text = correct
+	for i in $przyciski.get_children():
+		i.connect("pressed", self, "addToBuffer", [i.name])
+		
+func addToBuffer(x):
+	buffer = buffer + x
+	$input/Label.text = $input/Label.text + "*"
+	if buffer == correct:
+		$Timer.start()
+		yield($Timer, "timeout")
+		var TaskWithGUI = load("res://scripts/tasks/TaskWithGUI.cs")
+		TaskWithGUI.TaskWithGUICompleteTask(self)
+
+
+func _on_nie_pressed():
+	buffer = ""
+	$input/Label.text = ""
---
+++ scripts/tasks/Haslo.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public class Haslo : TaskWithGUI
+{
+	public override string GetResourceLocation(){
+		return "res://gui/haslo.tscn";
+	}
+}
