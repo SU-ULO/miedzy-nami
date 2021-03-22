6152d26947b4fce15e15542298b037b2cc33dc13
PangolinGiLA <wojtek.j.malecha@gmail.com>
2021-03-22 12:30:17 +0100

    lobby changes

--- scripts/comms.gd
+++ scripts/comms.gd
@@ -7,8 +7,10 @@ onready var wave1_pos = get_node("Texture/wave1").global_position
 onready var wave2_pos = get_node("Texture/wave2").global_position
 onready var timer = get_node("Timer")
 
-const max_noise:float = 12.0
-const min_noise:float = -12.0
+const max_noise:float = 8.0
+const min_noise:float = -8.0
+const move = 10
+var x = 0
 
 var set:bool = false
 
@@ -19,11 +21,11 @@ func _ready():
 	if rng.randi_range(0, 1) == 0:
 		knob.full_rotations *= -1
 
-func _process(_delta):
+func _process(delta):
 	update()
-
+	x+=move*delta
 func check(noise):
-	if abs(noise) < 0.5: set = true
+	if abs(noise) < 0.1: set = true
 	else: set = false
 	
 	if set and timer.time_left == 0: timer.start()
@@ -33,15 +35,15 @@ func _draw():
 	var noise = knob.prevangle + 2*PI*knob.full_rotations
 	check(noise)
 	# some magic values
-	draw_sin(20.0, 21.0, 50.0, 15.0, wave1_pos, 0.5, Color("#00FF00"))
-	draw_sin(20.0, 21.0, 50.0, 15.0, wave2_pos, clamp(noise, min_noise, max_noise))
+	draw_sin(30.0, 14.0, 50.0, 5.0, wave1_pos, 0.5, Color("#00FF00"))
+	draw_sin(30.0, 14.0, 50.0, 5.0, wave2_pos, clamp(noise, min_noise, max_noise))
 
 func draw_sin(scale = 1, length = 500, resolution = 4, speed = 1, offset = Vector2(0, 0), noise_amount = 0, color = Color(1, 1, 1, 1)):
 	if abs(noise_amount) < 0.5: noise_amount = 0.5
 	var points = PoolVector2Array()
 	for i in range(resolution * length):
 		var p1raw = i / resolution
-		var p2 = sin(p1raw * speed) + rng.randf_range(0, 0.4) * noise_amount - 0.2 * noise_amount
+		var p2 = sin(p1raw * speed + x) + rng.randf_range(0, 0.4) * noise_amount - 0.2 * noise_amount
 		var p1 = p1raw
 		points.push_back(Vector2(p1, p2) * scale + offset)
 	for i in range(resolution * length - 1):
