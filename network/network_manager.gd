a676ea961362435fdb91c32c18aa73082e87580e
Sobgo <60814050+Sobgo@users.noreply.github.com>
2021-03-28 18:57:31 +0200

    no player gui on end screen

--- network/network_manager.gd
+++ network/network_manager.gd
@@ -431,6 +431,5 @@ func end_game(crew_win: bool):
 		
 	end_screen.get_node("Control/imps").text = "" # unused label
 	end_screen.get_node("Control").visible = true
-	own_player.get_node("CanvasLayer/playerGUI").setVisibility("self", 0)
 	self.add_child(end_screen)
 	endscreen=end_screen
