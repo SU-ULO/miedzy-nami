b1e7dee5e921032d2039867f00fb3eaf4ff7d968
Sobgo <60814050+Sobgo@users.noreply.github.com>
2021-04-04 01:11:36 +0200

    books drag and drop rework part 1

--- scripts/tasks/LIB-Books.gd
+++ scripts/tasks/LIB-Books.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 var toDrag = []
 var toSort = 9
-func _ready():
-	pass # Replace with function body.
+
+var dragged = null
 
 func checkForEnd():
 	if toSort == 0:
--- scripts/tasks/ksiazka.gd
+++ scripts/tasks/ksiazka.gd
@@ -1,64 +1,79 @@
-extends Area2D
+extends Sprite
 
 var clickDelta = Vector2()
-var pickedUp = false
+
 var inFront = false
 var inBox = false
-var main
+
+var main = null
+var own_area = null
 
 func _ready():
+	main = get_owner()
+	own_area = get_child(0)
+	
 	# warning-ignore:return_value_discarded
-	connect("input_event", self, "inputEvent")
+	own_area.connect("input_event", self, "inputEvent")
 	# warning-ignore:return_value_discarded
-	connect("area_entered", self, "isInBox")
+	own_area.connect("area_entered", self, "isInBox")
 	# warning-ignore:return_value_discarded
-	connect("area_exited", self, "notInBox")
-	main = get_parent().get_parent()
+	own_area.connect("area_exited", self, "notInBox")
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		if pickedUp:
-			get_parent().position = get_viewport().get_mouse_position() - clickDelta
-
+		if self == main.dragged:
+			self.position = get_viewport().get_mouse_position() - clickDelta
 
 func inputEvent(_viewport, event, _shape):
 	if event is InputEventScreenTouch || event is InputEventMouseButton:
 		if event.is_pressed():
-					get_parent().get_parent().toDrag.append(get_parent().get_index())
-					if get_parent().get_parent().toDrag.min() == get_parent().get_index():
-						pickedUp = true
-						if inBox && !inFront:
-							main.toSort+=1
-						clickDelta = (get_viewport().get_mouse_position() - get_parent().position)
-		else:
-			if pickedUp:
-				get_parent().get_parent().toDrag.clear()
-				pickedUp = false
-				if inBox && !inFront:
-					main.toSort-=1
-					main.checkForEnd()
-						
+			main.toDrag.append(self.get_index())
+			if main.toDrag.max() == self.get_index():
+				drag_on(self)
+		elif main.dragged != null:
+			main.toDrag.clear()
+			drag_off()
+			main.checkForEnd()
+
+func drag_on(body):
+	if main.dragged != null:
+		drag_off()
+	main.dragged = body
+	print("draging ", body.name)
+	if body.inBox && !body.inFront:
+		main.toSort += 1
+		print("+1, total = ", main.toSort)
+	clickDelta = (get_viewport().get_mouse_position() - body.position)
+
+func drag_off():
+	print("stopped draging ", main.dragged.name)
+	if main.dragged.inBox && !main.dragged.inFront:
+		main.toSort -= 1
+		print("-1, total = ", main.toSort)
+	main.dragged = null
+
 func isInBox(area):
-	if area.name == "szafka-bok":
+	if area.name == "shelf-front":
 		inFront = true
-	if area.name == "polka-blue" && self.name == "blue":
+	
+	if area.name == "shelf-blue" && own_area.name == "blue":
 		inBox = true
-
-	if area.name == "polka-red" && self.name == "red":
+	
+	if area.name == "shelf-red" && own_area.name == "red":
 		inBox = true
-
-	if area.name == "polka-green" && self.name == "green":
+	
+	if area.name == "shelf-green" && own_area.name == "green":
 		inBox = true
 
 func notInBox(area):
-	if area.name == "szafka-bok":
+	if area.name == "shelf-front":
 		inFront = false
-	if area.name == "polka-blue" && self.name == "blue":
+	
+	if area.name == "shelf-blue" && own_area.name == "blue":
 		inBox = false
-
-	if area.name == "polka-red" && self.name == "red":
+	
+	if area.name == "shelf-red" && own_area.name == "red":
 		inBox = false
-
-	if area.name == "polka-green" && self.name == "green":
+	
+	if area.name == "shelf-green" && own_area.name == "green":
 		inBox = false
-
