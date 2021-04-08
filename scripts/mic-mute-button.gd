a673b0ed18e2cf32d3753f6facb4fcd3a0ed808f
LifeHasNoPurpose <mikolaj.juda@gmail.com>
2021-04-08 10:15:14 +0200

    connected signals to mic icon

--- scripts/mic-mute-button.gd
+++ scripts/mic-mute-button.gd
@@ -2,8 +2,13 @@ extends TextureButton
 
 # 1 is mic off
 
-func set_mute(state: bool = false):
-	pressed = state
+func _ready():
+	connect("button_down", VoiceChat, "button_down")
+	connect("button_up", VoiceChat, "button_up")
+	VoiceChat.connect("speaking", self, "set_speaking")
+
+func set_speaking(state: bool = false):
+	pressed = !state
 	
 func get_mute():
 	return pressed
