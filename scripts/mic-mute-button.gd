3ed0ae7340fd5dd5fcd5e32067c4542def3764b6
LifeHasNoPurpose <mikolaj.juda@gmail.com>
2021-04-08 10:40:38 +0200

    working mic mute icon

--- scripts/mic-mute-button.gd
+++ scripts/mic-mute-button.gd
@@ -2,16 +2,17 @@ extends TextureButton
 
 # 1 is mic off
 
+var on_icon := preload("res://textures/mic-on.png")
+var off_icon := preload("res://textures/mic-off.png")
+
 func _ready():
+# warning-ignore:return_value_discarded
 	connect("button_down", VoiceChat, "button_down")
+# warning-ignore:return_value_discarded
 	connect("button_up", VoiceChat, "button_up")
+# warning-ignore:return_value_discarded
 	VoiceChat.connect("speaking", self, "set_speaking")
+	set_speaking(VoiceChat.isunmuted())
 
 func set_speaking(state: bool = false):
-	pressed = !state
-	
-func get_mute():
-	return pressed
-	
-func toggle():
-	pressed = !pressed
+	texture_normal = on_icon if state else off_icon
