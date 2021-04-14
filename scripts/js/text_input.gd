72df64fe1295b2292fe2b1d7a5c8bf0a17a9ed51
LifeHasNoPurpose <mikolaj.juda@gmail.com>
2021-04-14 23:25:56 +0200

    touchscreen input

--- scripts/js/text_input.gd
+++ scripts/js/text_input.gd
@@ -1,9 +1,21 @@
 extends LineEdit
 
+export var text_id := ""
+
 func _ready():
 	if TouchscreenKeyboard.available:
 # warning-ignore:return_value_discarded
 		connect("focus_entered", self, "js_text_entry")
+# warning-ignore:return_value_discarded
+		connect("focus_exited", TouchscreenKeyboard, "end_input")
+# warning-ignore:return_value_discarded
+		TouchscreenKeyboard.connect("text", self, "handle_text_keyboard")
 
 func js_text_entry():
-	print("clicked")
+	TouchscreenKeyboard.start_input(text_id, text)
+
+func handle_text_keyboard(keyboardtext: String, id: String):
+	if id==text_id:
+		text=keyboardtext
+	else:
+		release_focus()
