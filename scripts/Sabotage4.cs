75e3e22a09ffd6ba4cea4cd010f720cfd0d36ee4
NNX <24442148+TheNNX@users.noreply.github.com>
2021-04-15 19:19:32 +0200

    Fix sabotage 4 (part 1)
    I didn't want to break school.tscn, it will be included in the next commit.

--- scripts/Sabotage4.cs
+++ scripts/Sabotage4.cs
@@ -3,9 +3,6 @@ using System;
 
 public class Sabotage4 : Node2D, IInteractable
 {
-	public static int currentlyPressed = 0;
-	public static int needed = 2;
-	public static bool signalAttached = false;
 	
 	public override void _Ready(){
 		AddToGroup("entities");
@@ -13,34 +10,6 @@ public class Sabotage4 : Node2D, IInteractable
 		Node network = (Node)GetTree().GetRoot().GetNode("Start").Get("network");
 		network.Connect("sabotage", this, "SetInteractable");
 		network.Connect("sabotage_end", this, "SetInteractable");
-		if(signalAttached == false)
-		{
-			signalAttached = true;
-			network.Connect("gui_sync", this, "HandleGUISync");
-		}
-	}
-	
-	public void HandleGUISync(string guiName, Godot.Collections.Dictionary<String, object> data){
-		Node network = (Node)GetTree().GetRoot().GetNode("Start").Get("network");
-		if(guiName == "sabotage4"){
-			Godot.GD.Print(data.Keys.Count, data.Count);
-			foreach (String s in data.Keys){
-				Godot.GD.Print("key: " + s);
-			}
-			
-			if(data.ContainsKey("currentlyPressedDelta")){
-				currentlyPressed += (int)data["currentlyPressedDelta"];
-				Godot.GD.Print("Delta: " + (int)data["currentlyPressedDelta"]);
-			}
-			if(data.ContainsKey("currentlyPressed")){
-				currentlyPressed = (int)data["currentlyPressed"];
-								Godot.GD.Print("Setting total to " + currentlyPressed);
-			}
-			if(currentlyPressed >= needed){
-				network.Call("request_end_sabotage", 4);
-				Godot.GD.Print("state: "+currentlyPressed);
-				}
-		}
 	}
 
 	public bool Interact(Node2D body){
