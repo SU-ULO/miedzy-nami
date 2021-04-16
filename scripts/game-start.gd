96909ddd8152f095a9d416e5b7cfd30785d63078
TheNNX <nnxcomputing@gmail.com>
2021-04-16 13:08:55 +0200

    Remove an unused scene

--- scripts/game-start.gd
+++ scripts/game-start.gd
@@ -1,7 +1,5 @@
 extends StaticBody2D
 
-var loadingscreen = preload("res://gui/loadingscreen.tscn")
-var x
 var heh
 
 func _ready():
@@ -17,18 +15,5 @@ func Interact(body):
 			net.request_game_start()
 	return false
 
-func display_start_ui():
-	get_heh()
-	x = loadingscreen.instance()
-	x.connect("finish", self, "letsgo")
-	heh.get_node("CanvasLayer/playerGUI").visible = false
-	heh.get_parent().get_parent().get_parent().get_node("CanvasLayer").add_child(x)
-
 func EndInteraction(_body):
 	pass
-
-func letsgo():
-	x.disconnect("finish", self, "letsgo")
-	x.queue_free()
-	heh.get_node("CanvasLayer/playerGUI").visible = true
-	heh.currentInteraction = null
