57987de855905f8a847612221e0135eef6342303
Sobgo <60814050+Sobgo@users.noreply.github.com>
2022-04-21 19:49:01 +0200

    suwmiarka fix

--- scripts/tasks/PHY-Sto.gd
+++ scripts/tasks/PHY-Sto.gd
@@ -1,30 +1,43 @@
 extends Control
 
-var pos = 0
-var pos2 = 0
 var started = false
+
+var start_pos = 0 # positions on animation timeline
+var end_pos = 0
+var err_margin = 0.1 # fraction of total animation length
+var anim_length
+var timeout = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("AnimationPlayer").play("wahadlo")
+	anim_length = get_node("AnimationPlayer").current_animation_length
 
-
-func _on_TextureButton_pressed():
+func _on_Timer_pressed():
 	get_node("AnimationPlayer2").play("clock-tick")
-	if !started:
-		started = true
-		pos = get_node("AnimationPlayer").current_animation_position	
-	else:
-		started = false
+	
+	if !started: # start
+		start_pos = get_node("AnimationPlayer").current_animation_position
+		timeout = false
+		get_node("Timer").start(2)
+	else: # stop
 		get_node("AnimationPlayer2").stop()
-		pos2 = get_node("AnimationPlayer").current_animation_position
-		if abs(pos - pos2) < 0.1 || abs(pos - (get_node("AnimationPlayer").current_animation_length - pos2)) < 0.1:
-			print("Gratulacje!") # only for testing
-			get_node("AnimationPlayer2").play("wait")
-		else:
-			#ups zrob to jeszcze raz (jak to zasygnalizowac graczowi??)
-			print("Nie") #only for testing
+		end_pos = get_node("AnimationPlayer").current_animation_position
+		check_win()
+	started = !started
 
+func check_win():
+	if min(abs(end_pos - start_pos), abs(end_pos - start_pos - anim_length)) < anim_length*err_margin \
+	and timeout:
+		get_node("AnimationPlayer2").play("wait")
+		print("OK")
+	else: 
+		get_node("AnimationPlayer2").play("quick-spin")
 
 func _on_AnimationPlayer2_animation_finished(anim_name):
 	if anim_name == "wait":
 		TaskWithGUI.TaskWithGUICompleteTask(self)
+
+
+func _on_Timer_timeout():
+	timeout = true
