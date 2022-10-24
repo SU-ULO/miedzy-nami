47bbd4243077108b1ca83692f81bbdf273321951
mikolajjuda <mikolaj.juda@gmail.com>
2022-10-24 20:25:09 +0200

    something

--- scripts/navpoint.gd
+++ scripts/navpoint.gd
@@ -6,10 +6,10 @@ enum NavpointType {ROOM_NAVPOINT, PATH_NAVPOINT}
 enum LookDirection {UP, DOWN, LEFT, RIGHT}
 
 export(NavpointType) var type = NavpointType.ROOM_NAVPOINT
-export(float) var idling_chance = 0
+export(float) var idling_chance = 0.0
 export(LookDirection) var look_dir = LookDirection.DOWN
-export(float) var wait_time = 2
-export(float) var skip_chance = 0
+export(float) var wait_time = 2.0
+export(float) var skip_chance = 0.0
 
 func _ready():
 	add_to_group("entities")
